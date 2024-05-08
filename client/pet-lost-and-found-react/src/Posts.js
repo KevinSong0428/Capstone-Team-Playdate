01ed776dc806f9f66912bc04eb77c420ab8b4a1e
BrandonW-Lee <69485815+BrandonW-Lee@users.noreply.github.com>
2024-05-07 22:40:49 -0700

    oh jesus so many branches

--- client/pet-lost-and-found-react/src/Posts.js
+++ client/pet-lost-and-found-react/src/Posts.js
@@ -1,49 +1,61 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 export default function Posts() {
-    const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
 
-    const url = "http://localhost:8080/api/post";
+  const url = "http://localhost:8080/api/post";
 
-    useEffect(() => {
-        fetch(url)
-            .then(response => {
-                if (response.status === 200) {
-                    return response.json();
-                } else {
-                    return Promise.reject(`Unexpected status code: ${response.status}`);
-                }
-            })
-            .then(data => setPosts(data)) // here we are setting our data to our state variable
-            .catch(console.log);
-    }, []);
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        } else {
+          return Promise.reject(`Unexpected status code: ${response.status}`);
+        }
+      })
+      .then((data) => setPosts(data)) // here we are setting our data to our state variable
+      .catch(console.log);
+  }, []);
 
-    return (
-        <>
-            <div className="container mt-5">
-                <div className="row">
-                    <h2 className="flex-grow">All Posts</h2>
+  return (
+    <>
+      <div className='container mt-5'>
+        <div className='row'>
+          <h2 className='flex-grow'>All Posts</h2>
+        </div>
+        <div className='row'>
+          {posts.map((post) => (
+            <div
+              key={post.postId}
+              className='col-lg-4 mb-4'
+            >
+              <div className='card'>
+                <div className='card-bod'>
+                  <h5 className='card-title'>
+                    {post.found ? "FOUND" : "LOST"}: {post.animal.name}{" "}
+                  </h5>
+                  <p className='card-text'>
+                    <strong>Description: </strong>
+                    {post.description}
+                    <br />
+                    <strong>Found: </strong>
+                    {post.dateTime}
+                    <br />
+                    <strong>Gender: </strong>
+                    {post.gender}
+                    <br />
+                    <strong>Contact me at: </strong>
+                    {post.user.phoneNumber} or {post.user.email}
+                    <br />
+                  </p>
                 </div>
-                <div className="row">
-                    {posts.map(post => (
-                        <div key={post.postId} className="col-lg-4 mb-4">
-                            <div className="card">
-                                <div className="card-bod">
-                                    <h5 className="card-title">{post.found ? "FOUND" : "LOST"}: {post.animal.name} </h5>
-                                    <p className="card-text">
-                                        <strong>Description: </strong>{post.description}<br />
-                                        <strong>Found: </strong>{post.dateTime}<br />
-                                        <strong>Gender: </strong>{post.gender}<br />
-                                        <strong>Contact me at: </strong>{post.user.phoneNumber} or {post.user.email}<br />
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
-                </div>
+              </div>
             </div>
-        </>
-    )
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
-
---
+++ client/pet-lost-and-found-react/src/components/Carousel/index.jsx
@@ -0,0 +1,35 @@
+// "use client";
+
+// import Images from "./Image";
+
+// export default function Carousel() {
+//   return (
+//     <Swiper
+//       autoplay={{
+//         delay: 1000,
+//       }}
+//       effect='fade'
+//       loop={true}
+//       modules={[Autoplay, A11y, EffectFade]}
+//     >
+//       <SwiperSlide>
+//         <Images
+//           alt='A super happy dog'
+//           src='/pet-lost-and-found-react/public/photos/Dog1.jpg'
+//         />
+//       </SwiperSlide>
+//       <SwiperSlide>
+//         <Images
+//           alt='good boy gets scritches'
+//           src='/pet-lost-and-found-react/public/photos/Dog1.jpg'
+//         />
+//       </SwiperSlide>
+//       <SwiperSlide>
+//         <Images
+//           alt='The cutest husky?'
+//           src='/pet-lost-and-found-react/public/photos/Dog2.jpg'
+//         />
+//       </SwiperSlide>
+//     </Swiper>
+//   );
+// }
